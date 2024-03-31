0127c7ef2d6fb2171de70e0366c94bb5812d1283
DANIAFARHAN <daniaimtiaz06@gmail.com>
2024-03-31 07:30:32 +0500

    Dania well done, Parvez Catering wala with you, keep it up

---
+++ Zakat.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let zakat = 2.5;
+let goldValueInTola = 234800;
+let goldWeightInTolaReq = 7.5;
+let condition = true;
+const answer = await inquirer.prompt([
+    {
+        name: "goldWeight",
+        massage: "EntergoldWeight",
+        type: "number",
+    }
+]);
+let goldWeight = answer.goldWeight;
+if (goldWeight >= goldWeightInTolaReq) {
+    let calculatedzakat = (goldWeight * goldValueInTola) * (zakat / 100);
+    console.log(`The calculated value of gold weighing 
+    ${goldWeight} tola ${calculatedzakat}`);
+}
+else {
+    console.log("zakat not applicable");
+}
+condition = false;
